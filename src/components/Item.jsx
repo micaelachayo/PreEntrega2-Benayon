b268a285c34038f2bea6448c7147bb4056c5797c
micaelachayo <micaelachayo@gmail.com>
2024-04-01 23:45:12 -0300

    modificando

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -1,20 +1,18 @@
-
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import { Link } from 'react-router-dom';
-import '../app.css';
 
-    
-export const Item = ({product}) =>{
-    return (
-  
-        <Card className='card' >
-          <Card.Img className='card-img' variant="top" src={product.pictureUrl} />
-          <Card.Body className='card-body'>
-            <Card.Title>{product.title}</Card.Title>
-            <strong >${product.price}</strong>
-            <Link className='boton' to={`/item/${product.id}`}><Button >Ver Producto</Button></Link>
-          </Card.Body>
-        </Card>
-    );
-  }
+export const Item = ({ product }) => {
+  return (
+    <Card className='card'>
+      <Card.Img className='card-img' variant="top" src={product.pictureUrl} />
+      <Card.Body>
+        <Card.Title>{product.title}</Card.Title>
+        <Card.Text>{product.price}</Card.Text>
+        <Link to={`/product/${product.id}`}>
+          <Link to={`/item/${product.id}`}><Button variant="primary">Ver más</Button></Link>
+        </Link>
+      </Card.Body>
+    </Card>
+  );
+};
